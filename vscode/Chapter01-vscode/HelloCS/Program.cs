347b1c25702b95bc1424b24a7970ebc1be4189c0
Neyah <neyahj@aol.com>
2025-01-24 17:23:28 -0500

    Added Program.cs and updated lab files

--- vscode/Chapter01-vscode/HelloCS/Program.cs
+++ vscode/Chapter01-vscode/HelloCS/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#!");
-throw new Exception();
-int z;
+// throw new Exception();
+
+int z = 42; // Assign a value to the variable
+Console.WriteLine($"The value of z is: {z}");
